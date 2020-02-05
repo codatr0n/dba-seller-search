ef84cbbef434471ee74a83195f91a9c1f65c9abe
Erik MK <inboxtoerik@gmail.com>
2020-02-05 21:48:00 +0100

    frontend added + json response from dba

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('get_listings', views.get_listings, name='get_listings'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,8 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+import requests
+import json
 
 
 def home(request):
     return render(request, 'base.html')
+
+
+def get_listings(request):
+    seller_url = request.POST.get('seller_url')
+
+    headers = {
+        'Host': 'www.dba.dk',
+        'User-Agent':
+        'Mozilla/5.0 (Macintosh Intel Mac OS X 10.15 rv: 71.0) Gecko/20100101 Firefox/71.0',
+        'Accept': 'text/html, application/xhtml+xml, application/xml'
+    }
+
+    start_url = seller_url
+
+    api_url = 'https://www.dba.dk/api/dba-soi-bff/saelger/privat/dba/{seller_id}?page='.format(
+        seller_id=start_url.split('/')[-1])
+
+    nextPage = '1'
+
+    while nextPage:
+        response = requests.get(api_url + str(nextPage), headers=headers)
+        listings = response.json()
+
+        for listing in listings['listings']['listings']:
+            # print(listing)
+            first_line = listing['text'].split('\n')[0]
+            category = first_line.split(',')[0]
+            if category in ['LP', 'EP', 'Maxi-single 12"', 'Single']:
+                item = {}
+                item['price'] = listing['price']['value']
+                item['image'] = listing['pictureUrl']
+                item['url'] = listing['url']
+                item['category'] = first_line.split(',')[0]
+                item['artist'] = first_line.split(',')[1].strip()
+                item['title'] = first_line.split(',')[2].strip()
+
+                # print(listing['text'].split('\n')[0])
+                print(json.dumps(item, indent=4))
+
+        # nextPage = listings['listings']['nextPage']
+        nextPage = 0
+
+    return HttpResponse('Listings')
